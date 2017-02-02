645306381dda8a121065a22b0a62885705c6fa31
Taras Leskiv <taras@getsocial.im>
2017-02-02 13:54:34 +0100

    init

---
+++ Scripts/CallJniExtensionMethods.cs
@@ -0,0 +1,123 @@
+﻿#if UNITY_ANDROID
+using UnityEngine;
+
+namespace DeadMosquito.JniToolkit
+{
+    static class CallJniExtensionMethods
+    {
+
+        public static string GetClassName(this AndroidJavaObject ajo)
+        {
+            return ajo.GetJavaClass().Call<string>("getName");
+        }
+
+        public static string GetClassSimpleName(this AndroidJavaObject ajo)
+        {
+            return ajo.GetJavaClass().Call<string>("getSimpleName");
+        }
+
+        public static AndroidJavaObject GetJavaClass(this AndroidJavaObject ajo)
+        {
+            return ajo.CallAJO("getClass");
+        }
+
+        public static string JavaToString(this AndroidJavaObject ajo)
+        {
+            return ajo.Call<string>("toString");
+        }
+
+        #region AndroidJavaObject_Call_Proxy
+
+        public static string CallStr(this AndroidJavaObject ajo, string methodName, params object[] args)
+        {
+            return ajo.Call<string>(methodName, args);
+        }
+
+        public static bool CallBool(this AndroidJavaObject ajo, string methodName, params object[] args)
+        {
+            return ajo.Call<bool>(methodName, args);
+        }
+
+        public static float CallFloat(this AndroidJavaObject ajo, string methodName, params object[] args)
+        {
+            return ajo.Call<float>(methodName, args);
+        }
+
+        public static int CallInt(this AndroidJavaObject ajo, string methodName, params object[] args)
+        {
+            return ajo.Call<int>(methodName, args);
+        }
+
+        public static long CallLong(this AndroidJavaObject ajo, string methodName, params object[] args)
+        {
+            return ajo.Call<long>(methodName, args);
+        }
+
+        public static string CallStaticStr(this AndroidJavaObject ajo, string methodName, params object[] args)
+        {
+            return ajo.CallStatic<string>(methodName, args);
+        }
+
+        public static AndroidJavaObject CallAJO(this AndroidJavaObject ajo, string methodName, params object[] args)
+        {
+            return ajo.Call<AndroidJavaObject>(methodName, args);
+        }
+
+        public static AndroidJavaObject CallStaticAJO(this AndroidJavaObject ajo, string methodName, params object[] args)
+        {
+            return ajo.CallStatic<AndroidJavaObject>(methodName, args);
+        }
+
+        #endregion
+
+        #region AndroidJavaClass_Call_Proxy
+
+        // GET STATIC
+        public static string GetStaticStr(this AndroidJavaClass ajc, string fieldName)
+        {
+            return ajc.GetStatic<string>(fieldName);
+        }
+
+        public static bool GetStaticBool(this AndroidJavaClass ajc, string fieldName)
+        {
+            return ajc.GetStatic<bool>(fieldName);
+        }
+
+        public static int GetStaticInt(this AndroidJavaClass ajc, string fieldName)
+        {
+            return ajc.GetStatic<int>(fieldName);
+        }
+
+        // CALL STATIC
+        public static string CallStaticStr(this AndroidJavaClass ajc, string methodName, params object[] args)
+        {
+            return ajc.CallStatic<string>(methodName, args);
+        }
+
+        public static int CallStaticInt(this AndroidJavaClass ajc, string methodName, params object[] args)
+        {
+            return ajc.CallStatic<int>(methodName, args);
+        }
+
+        public static long CallStaticLong(this AndroidJavaClass ajc, string methodName, params object[] args)
+        {
+            return ajc.CallStatic<long>(methodName, args);
+        }
+
+        public static AndroidJavaObject CallStaticAJO(this AndroidJavaClass ajc, string methodName, params object[] args)
+        {
+            return ajc.CallStatic<AndroidJavaObject>(methodName, args);
+        }
+
+        public static T AJCCallStaticOnce<T>(this string className, string methodName, params object[] args)
+        {
+            using (var ajc = new AndroidJavaClass(className))
+            {
+                return ajc.CallStatic<T>(methodName, args);
+            }
+        }
+
+        #endregion
+    }
+}
+#endif
---
+++ Scripts/JniToolkitUtils.cs
@@ -0,0 +1,144 @@
+﻿#if UNITY_ANDROID
+using UnityEngine;
+using System;
+
+namespace DeadMosquito.JniToolkit
+{
+    public static class JniToolkitUtils
+    {
+        const string JavaLangSystemClass = "java.lang.System";
+
+        static AndroidJavaObject _activity;
+
+        public static AndroidJavaObject Activity
+        {
+            get
+            {
+                if (_activity == null)
+                {
+                    var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+                    _activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+                }
+                return _activity;
+            }
+        }
+
+        public static AndroidJavaObject ActivityDecorView
+        {
+            get
+            {
+                return Activity.Call<AndroidJavaObject>("getWindow").Call<AndroidJavaObject>("getDecorView");
+            }
+        }
+
+        public static AndroidJavaObject PackageManager
+        {
+            get { return Activity.CallAJO("getPackageManager"); }
+        }
+
+        public static AndroidJavaObject ContentResolver
+        {
+            get { return JniToolkitUtils.Activity.CallAJO("getContentResolver"); }
+        }
+
+        public static bool HasSystemFeature(string feature)
+        {
+            using (var pm = PackageManager)
+            {
+                return pm.CallBool("hasSystemFeature", feature);
+            }
+        }
+
+        public static long CurrentTimeMillis
+        {
+            get
+            {
+                using (var system = new AndroidJavaClass(JavaLangSystemClass))
+                {
+                    return system.CallStaticLong("currentTimeMillis");
+                }
+            }
+        }
+
+        #region reflection
+
+        public static AndroidJavaObject ClassForName(string className)
+        {
+            using (var clazz = new AndroidJavaClass("java.lang.Class"))
+            {
+                return clazz.CallStaticAJO("forName", className);
+            }
+        }
+
+        public static AndroidJavaObject Cast(this AndroidJavaObject source, string destClass)
+        {
+            using (var destClassAJC = ClassForName(destClass))
+            {
+                return destClassAJC.Call<AndroidJavaObject>("cast", source);
+            }
+        }
+
+        public static bool IsJavaNull(this AndroidJavaObject ajo)
+        {
+            return ajo.GetRawObject().ToInt32() == 0;
+        }
+
+        #endregion
+
+        public static bool IsNotAndroidCheck()
+        {
+            bool isAndroid = Application.platform == RuntimePlatform.Android;
+
+            return !isAndroid;
+        }
+
+        public static void RunOnUiThread(Action action)
+        {
+            Activity.Call("runOnUiThread", new AndroidJavaRunnable(action));
+        }
+
+        public static void StartActivity(AndroidJavaObject intent, Action fallback = null)
+        {
+            try
+            {
+                Activity.Call("startActivity", intent);
+            }
+            catch (AndroidJavaException exception)
+            {
+                if (Debug.isDebugBuild)
+                {
+                    Debug.LogWarning("Could not start the activity with " + intent.JavaToString() + ": " + exception.Message);
+                }
+                if (fallback != null)
+                    fallback();
+            }
+            finally
+            {
+                intent.Dispose();
+            }
+        }
+
+        public static void StartActivityWithChooser(AndroidJavaObject intent, string chooserTitle)
+        {
+            try
+            {
+                AndroidJavaObject jChooser = intent.CallStaticAJO("createChooser", intent, chooserTitle);
+                Activity.Call("startActivity", jChooser);
+            }
+            catch (AndroidJavaException exception)
+            {
+                Debug.LogWarning("Could not start the activity with " + intent.JavaToString() + ": " + exception.Message);
+            }
+            finally
+            {
+                intent.Dispose();
+            }
+        }
+
+        public static void SendBroadcast(AndroidJavaObject intent)
+        {
+            Activity.Call("sendBroadcast", intent);
+        }
+    }
+}
+#endif
