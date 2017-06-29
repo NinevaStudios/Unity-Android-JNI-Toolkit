55fdeb5ed9652ebf1c68a24dd0cce282d51a7784
Taras Leskiv <leskiv.taras@gmail.com>
2017-06-29 22:02:54 +0200

    cleanup

--- Scripts/CallJniExtensionMethods.cs
+++ Scripts/CallJniExtensionMethods.cs
@@ -3,209 +3,215 @@ using System;
 
 namespace DeadMosquito.JniToolkit
 {
-    static class CallJniExtensionMethods
-    {
+	static class CallJniExtensionMethods
+	{
+		public static string GetClassName(this AndroidJavaObject ajo)
+		{
+			return ajo.GetJavaClass().Call<string>("getName");
+		}
 
-        public static string GetClassName(this AndroidJavaObject ajo)
-        {
-            return ajo.GetJavaClass().Call<string>("getName");
-        }
+		public static string GetClassSimpleName(this AndroidJavaObject ajo)
+		{
+			return ajo.GetJavaClass().Call<string>("getSimpleName");
+		}
 
-        public static string GetClassSimpleName(this AndroidJavaObject ajo)
-        {
-            return ajo.GetJavaClass().Call<string>("getSimpleName");
-        }
+		public static AndroidJavaObject GetJavaClass(this AndroidJavaObject ajo)
+		{
+			return ajo.CallAJO("getClass");
+		}
 
-        public static AndroidJavaObject GetJavaClass(this AndroidJavaObject ajo)
-        {
-            return ajo.CallAJO("getClass");
-        }
+		public static string JavaToString(this AndroidJavaObject ajo)
+		{
+			return ajo.Call<string>("toString");
+		}
 
-        public static string JavaToString(this AndroidJavaObject ajo)
-        {
-            return ajo.Call<string>("toString");
-        }
+		#region AndroidJavaObject_Call_Proxy
 
-        #region AndroidJavaObject_Call_Proxy
+		public static string CallStr(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.Call<string>(methodName, args);
+		}
 
-        public static string CallStr(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.Call<string>(methodName, args);
-        }
+		public static bool CallBool(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.Call<bool>(methodName, args);
+		}
 
-        public static bool CallBool(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.Call<bool>(methodName, args);
-        }
+		public static float CallFloat(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.Call<float>(methodName, args);
+		}
 
-        public static float CallFloat(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.Call<float>(methodName, args);
-        }
+		public static double CallDouble(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.Call<double>(methodName, args);
+		}
 
-        public static double CallDouble(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.Call<double>(methodName, args);
-        }
+		public static int CallInt(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.Call<int>(methodName, args);
+		}
 
-        public static int CallInt(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.Call<int>(methodName, args);
-        }
+		public static long CallLong(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.Call<long>(methodName, args);
+		}
 
-        public static long CallLong(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.Call<long>(methodName, args);
-        }
+		public static string CallStaticStr(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.CallStatic<string>(methodName, args);
+		}
 
-        public static string CallStaticStr(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.CallStatic<string>(methodName, args);
-        }
+		public static AndroidJavaObject CallAJO(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.Call<AndroidJavaObject>(methodName, args);
+		}
 
-        public static AndroidJavaObject CallAJO(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.Call<AndroidJavaObject>(methodName, args);
-        }
+		public static AndroidJavaObject CallStaticAJO(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.CallStatic<AndroidJavaObject>(methodName, args);
+		}
 
-        public static AndroidJavaObject CallStaticAJO(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.CallStatic<AndroidJavaObject>(methodName, args);
-        }
+		#endregion
 
-        #endregion
+		#region AndroidJavaClass_Call_Proxy
 
-        #region AndroidJavaClass_Call_Proxy
+		// GET STATIC
+		public static string GetStaticStr(this AndroidJavaClass ajc, string fieldName)
+		{
+			return ajc.GetStatic<string>(fieldName);
+		}
 
-        // GET STATIC
-        public static string GetStaticStr(this AndroidJavaClass ajc, string fieldName)
-        {
-            return ajc.GetStatic<string>(fieldName);
-        }
+		public static bool GetStaticBool(this AndroidJavaClass ajc, string fieldName)
+		{
+			return ajc.GetStatic<bool>(fieldName);
+		}
 
-        public static bool GetStaticBool(this AndroidJavaClass ajc, string fieldName)
-        {
-            return ajc.GetStatic<bool>(fieldName);
-        }
+		public static int GetStaticInt(this AndroidJavaClass ajc, string fieldName)
+		{
+			return ajc.GetStatic<int>(fieldName);
+		}
 
-        public static int GetStaticInt(this AndroidJavaClass ajc, string fieldName)
-        {
-            return ajc.GetStatic<int>(fieldName);
-        }
+		// CALL STATIC
+		public static string CallStaticStr(this AndroidJavaClass ajc, string methodName, params object[] args)
+		{
+			return ajc.CallStatic<string>(methodName, args);
+		}
 
-        // CALL STATIC
-        public static string CallStaticStr(this AndroidJavaClass ajc, string methodName, params object[] args)
-        {
-            return ajc.CallStatic<string>(methodName, args);
-        }
+		public static int CallStaticInt(this AndroidJavaClass ajc, string methodName, params object[] args)
+		{
+			return ajc.CallStatic<int>(methodName, args);
+		}
 
-        public static int CallStaticInt(this AndroidJavaClass ajc, string methodName, params object[] args)
-        {
-            return ajc.CallStatic<int>(methodName, args);
-        }
+		public static long CallStaticLong(this AndroidJavaClass ajc, string methodName, params object[] args)
+		{
+			return ajc.CallStatic<long>(methodName, args);
+		}
 
-        public static long CallStaticLong(this AndroidJavaClass ajc, string methodName, params object[] args)
-        {
-            return ajc.CallStatic<long>(methodName, args);
-        }
+		public static AndroidJavaObject CallStaticAJO(this AndroidJavaClass ajc, string methodName, params object[] args)
+		{
+			return ajc.CallStatic<AndroidJavaObject>(methodName, args);
+		}
 
-        public static AndroidJavaObject CallStaticAJO(this AndroidJavaClass ajc, string methodName, params object[] args)
-        {
-            return ajc.CallStatic<AndroidJavaObject>(methodName, args);
-        }
+		public static T AJCCallStaticOnce<T>(this string className, string methodName, params object[] args)
+		{
+			using (var ajc = new AndroidJavaClass(className))
+			{
+				return ajc.CallStatic<T>(methodName, args);
+			}
+		}
 
-        public static T AJCCallStaticOnce<T>(this string className, string methodName, params object[] args)
-        {
-            using (var ajc = new AndroidJavaClass(className))
-            {
-                return ajc.CallStatic<T>(methodName, args);
-            }
-        }
+		#endregion
 
-        #endregion
+		#region Filed_Get_Proxy
 
-        #region Filed_Get_Proxy
+		public static float GetFloat(this AndroidJavaObject ajo, string fieldName)
+		{
+			return ajo.Get<float>(fieldName);
+		}
 
-        public static float GetFloat(this AndroidJavaObject ajo, string fieldName)
-        {
-            return ajo.Get<float>(fieldName);
-        }
+		public static double GetDouble(this AndroidJavaObject ajo, string fieldName)
+		{
+			return ajo.Get<double>(fieldName);
+		}
 
-        public static double GetDouble(this AndroidJavaObject ajo, string fieldName)
-        {
-            return ajo.Get<double>(fieldName);
-        }
-        
-        public static AndroidJavaObject GetAJO(this AndroidJavaObject ajo, string fieldName)
-        {
-            return ajo.Get<AndroidJavaObject>(fieldName);
-        }
-        #endregion
+		public static AndroidJavaObject GetAJO(this AndroidJavaObject ajo, string fieldName)
+		{
+			return ajo.Get<AndroidJavaObject>(fieldName);
+		}
 
-        #region main_thread_get
-        public static void MainThreadCallNonBlocking<T>(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            JniToolkitUtils.RunOnUiThread(() => ajo.Call<T>(methodName, args));
-        }
+		#endregion
 
-        public static void MainThreadCallNonBlocking(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            JniToolkitUtils.RunOnUiThread(() => ajo.Call(methodName, args));
-        }
+		#region main_thread_get
 
-        public static T MainThreadCall<T>(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            T result = default(T);
-            bool wasSet = false;
-            JniToolkitUtils.RunOnUiThread(() =>
-            {
-                result = ajo.Call<T>(methodName, args);
-                wasSet = true;
-            });
-            while (!wasSet) { }
-            return result;
-        }
+		public static void MainThreadCallNonBlocking<T>(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			JniToolkitUtils.RunOnUiThread(() => ajo.Call<T>(methodName, args));
+		}
 
-        public static void MainThreadCall(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            bool finished = false;
-            JniToolkitUtils.RunOnUiThread(() =>
-            {
-                ajo.Call(methodName, args);
-                finished = true;
-            });
-            while (!finished) { }
-        }
+		public static void MainThreadCallNonBlocking(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			JniToolkitUtils.RunOnUiThread(() => ajo.Call(methodName, args));
+		}
 
-        public static AndroidJavaObject MainThreadCallAJO(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.MainThreadCall<AndroidJavaObject>(methodName, args);
-        }
+		public static T MainThreadCall<T>(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			T result = default(T);
+			bool wasSet = false;
+			JniToolkitUtils.RunOnUiThread(() =>
+			{
+				result = ajo.Call<T>(methodName, args);
+				wasSet = true;
+			});
+			while (!wasSet)
+			{
+			}
+			return result;
+		}
 
-        public static int MainThreadCallInt(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.MainThreadCall<int>(methodName, args);
-        }
+		public static void MainThreadCall(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			bool finished = false;
+			JniToolkitUtils.RunOnUiThread(() =>
+			{
+				ajo.Call(methodName, args);
+				finished = true;
+			});
+			while (!finished)
+			{
+			}
+		}
 
-        public static float MainThreadCallFloat(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.MainThreadCall<float>(methodName, args);
-        }
+		public static AndroidJavaObject MainThreadCallAJO(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.MainThreadCall<AndroidJavaObject>(methodName, args);
+		}
 
-        public static double MainThreadCallDouble(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.MainThreadCall<double>(methodName, args);
-        }
+		public static int MainThreadCallInt(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.MainThreadCall<int>(methodName, args);
+		}
 
-        public static bool MainThreadCallBool(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.MainThreadCall<bool>(methodName, args);
-        }
+		public static float MainThreadCallFloat(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.MainThreadCall<float>(methodName, args);
+		}
 
-        public static string MainThreadCallStr(this AndroidJavaObject ajo, string methodName, params object[] args)
-        {
-            return ajo.MainThreadCall<string>(methodName, args);
-        }
-        #endregion
-    }
+		public static double MainThreadCallDouble(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.MainThreadCall<double>(methodName, args);
+		}
+
+		public static bool MainThreadCallBool(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.MainThreadCall<bool>(methodName, args);
+		}
+
+		public static string MainThreadCallStr(this AndroidJavaObject ajo, string methodName, params object[] args)
+		{
+			return ajo.MainThreadCall<string>(methodName, args);
+		}
+
+		#endregion
+	}
 }
--- Scripts/JniToolkitUtils.cs
+++ Scripts/JniToolkitUtils.cs
@@ -3,149 +3,140 @@ using System;
 
 namespace DeadMosquito.JniToolkit
 {
-    public static class JniToolkitUtils
-    {
-        const string JavaLangSystemClass = "java.lang.System";
+	public static class JniToolkitUtils
+	{
+		const string JavaLangSystemClass = "java.lang.System";
 
-        static AndroidJavaObject _activity;
+		static AndroidJavaObject _activity;
 
-        public static bool IsAndroidRuntime
-        {
-            get
-            {
-                return Application.platform == RuntimePlatform.Android;
-            }
-        }
+		public static bool IsAndroidRuntime
+		{
+			get { return Application.platform == RuntimePlatform.Android; }
+		}
 
-        public static bool IsNotAndroidRuntime
-        {
-            get
-            {
-                return !IsAndroidRuntime;
-            }
-        }
+		public static bool IsNotAndroidRuntime
+		{
+			get { return !IsAndroidRuntime; }
+		}
 
-        public static AndroidJavaObject Activity
-        {
-            get
-            {
-                if (_activity == null)
-                {
-                    var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-                    _activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-                }
-                return _activity;
-            }
-        }
+		public static AndroidJavaObject Activity
+		{
+			get
+			{
+				if (_activity == null)
+				{
+					var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+					_activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+				}
+				return _activity;
+			}
+		}
 
-        public static AndroidJavaObject ActivityDecorView
-        {
-            get
-            {
-                return Activity.Call<AndroidJavaObject>("getWindow").Call<AndroidJavaObject>("getDecorView");
-            }
-        }
+		public static AndroidJavaObject ActivityDecorView
+		{
+			get { return Activity.Call<AndroidJavaObject>("getWindow").Call<AndroidJavaObject>("getDecorView"); }
+		}
 
-        public static AndroidJavaObject PackageManager
-        {
-            get { return Activity.CallAJO("getPackageManager"); }
-        }
+		public static AndroidJavaObject PackageManager
+		{
+			get { return Activity.CallAJO("getPackageManager"); }
+		}
 
-        public static AndroidJavaObject ContentResolver
-        {
-            get { return JniToolkitUtils.Activity.CallAJO("getContentResolver"); }
-        }
+		public static AndroidJavaObject ContentResolver
+		{
+			get { return JniToolkitUtils.Activity.CallAJO("getContentResolver"); }
+		}
 
-        public static bool HasSystemFeature(string feature)
-        {
-            using (var pm = PackageManager)
-            {
-                return pm.CallBool("hasSystemFeature", feature);
-            }
-        }
+		public static bool HasSystemFeature(string feature)
+		{
+			using (var pm = PackageManager)
+			{
+				return pm.CallBool("hasSystemFeature", feature);
+			}
+		}
 
-        public static long CurrentTimeMillis
-        {
-            get
-            {
-                using (var system = new AndroidJavaClass(JavaLangSystemClass))
-                {
-                    return system.CallStaticLong("currentTimeMillis");
-                }
-            }
-        }
+		public static long CurrentTimeMillis
+		{
+			get
+			{
+				using (var system = new AndroidJavaClass(JavaLangSystemClass))
+				{
+					return system.CallStaticLong("currentTimeMillis");
+				}
+			}
+		}
 
-        #region reflection
+		#region reflection
 
-        public static AndroidJavaObject ClassForName(string className)
-        {
-            using (var clazz = new AndroidJavaClass("java.lang.Class"))
-            {
-                return clazz.CallStaticAJO("forName", className);
-            }
-        }
+		public static AndroidJavaObject ClassForName(string className)
+		{
+			using (var clazz = new AndroidJavaClass("java.lang.Class"))
+			{
+				return clazz.CallStaticAJO("forName", className);
+			}
+		}
 
-        public static AndroidJavaObject Cast(this AndroidJavaObject source, string destClass)
-        {
-            using (var destClassAJC = ClassForName(destClass))
-            {
-                return destClassAJC.Call<AndroidJavaObject>("cast", source);
-            }
-        }
+		public static AndroidJavaObject Cast(this AndroidJavaObject source, string destClass)
+		{
+			using (var destClassAJC = ClassForName(destClass))
+			{
+				return destClassAJC.Call<AndroidJavaObject>("cast", source);
+			}
+		}
 
-        public static bool IsJavaNull(this AndroidJavaObject ajo)
-        {
-            return ajo.GetRawObject().ToInt32() == 0;
-        }
+		public static bool IsJavaNull(this AndroidJavaObject ajo)
+		{
+			return ajo.GetRawObject().ToInt32() == 0;
+		}
 
-        #endregion
+		#endregion
 
-        public static void RunOnUiThread(Action action)
-        {
-            Activity.Call("runOnUiThread", new AndroidJavaRunnable(action));
-        }
+		public static void RunOnUiThread(Action action)
+		{
+			Activity.Call("runOnUiThread", new AndroidJavaRunnable(action));
+		}
 
-        public static void StartActivity(AndroidJavaObject intent, Action fallback = null)
-        {
-            try
-            {
-                Activity.Call("startActivity", intent);
-            }
-            catch (AndroidJavaException exception)
-            {
-                if (Debug.isDebugBuild)
-                {
-                    Debug.LogWarning("Could not start the activity with " + intent.JavaToString() + ": " + exception.Message);
-                }
-                if (fallback != null)
-                    fallback();
-            }
-            finally
-            {
-                intent.Dispose();
-            }
-        }
+		public static void StartActivity(AndroidJavaObject intent, Action fallback = null)
+		{
+			try
+			{
+				Activity.Call("startActivity", intent);
+			}
+			catch (AndroidJavaException exception)
+			{
+				if (Debug.isDebugBuild)
+				{
+					Debug.LogWarning("Could not start the activity with " + intent.JavaToString() + ": " + exception.Message);
+				}
+				if (fallback != null)
+					fallback();
+			}
+			finally
+			{
+				intent.Dispose();
+			}
+		}
 
-        public static void StartActivityWithChooser(AndroidJavaObject intent, string chooserTitle)
-        {
-            try
-            {
-                AndroidJavaObject jChooser = intent.CallStaticAJO("createChooser", intent, chooserTitle);
-                Activity.Call("startActivity", jChooser);
-            }
-            catch (AndroidJavaException exception)
-            {
-                Debug.LogWarning("Could not start the activity with " + intent.JavaToString() + ": " + exception.Message);
-            }
-            finally
-            {
-                intent.Dispose();
-            }
-        }
+		public static void StartActivityWithChooser(AndroidJavaObject intent, string chooserTitle)
+		{
+			try
+			{
+				AndroidJavaObject jChooser = intent.CallStaticAJO("createChooser", intent, chooserTitle);
+				Activity.Call("startActivity", jChooser);
+			}
+			catch (AndroidJavaException exception)
+			{
+				Debug.LogWarning("Could not start the activity with " + intent.JavaToString() + ": " + exception.Message);
+			}
+			finally
+			{
+				intent.Dispose();
+			}
+		}
 
-        public static void SendBroadcast(AndroidJavaObject intent)
-        {
-            Activity.Call("sendBroadcast", intent);
-        }
-    }
+		public static void SendBroadcast(AndroidJavaObject intent)
+		{
+			Activity.Call("sendBroadcast", intent);
+		}
+	}
 }
