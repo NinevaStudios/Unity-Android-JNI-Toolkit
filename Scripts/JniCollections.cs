8ac4d070e9461544e54b20811209d131464f74e6
Taras Leskiv <leskiv.taras@gmail.com>
2017-07-06 21:46:31 +0200

    collections

---
+++ Scripts/JniCollections.cs
@@ -0,0 +1,95 @@
+﻿namespace DeadMosquito.JniToolkit
+{
+	using System;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public static class JniCollections
+	{
+		public static List<T> FromJavaList<T>(this AndroidJavaObject javaList)
+		{
+			if (javaList == null || javaList.IsJavaNull())
+			{
+				return new List<T>();
+			}
+
+			int size = javaList.CallInt("size");
+			var list = new List<T>(size);
+
+			for (int i = 0; i < size; i++)
+			{
+				var listItem = javaList.Call<T>("get", i);
+				list.Add(listItem);
+			}
+
+			javaList.Dispose();
+			return list;
+		}
+
+		public static List<T> FromJavaList<T>(this AndroidJavaObject javaList, Func<AndroidJavaObject, T> converter)
+		{
+			if (javaList == null || javaList.IsJavaNull())
+			{
+				return new List<T>();
+			}
+
+			int size = javaList.CallInt("size");
+			var list = new List<T>(size);
+
+			for (int i = 0; i < size; i++)
+			{
+				var listItem = converter(javaList.CallAJO("get", i));
+				list.Add(listItem);
+			}
+
+			javaList.Dispose();
+			return list;
+		}
+
+		public static Dictionary<string, object> FromJavaMap(this AndroidJavaObject javaMap)
+		{
+			if (javaMap == null || javaMap.IsJavaNull())
+			{
+				return new Dictionary<string, object>();
+			}
+
+			int size = javaMap.CallInt("size");
+			var dictionary = new Dictionary<string, object>(size);
+
+			var iterator = javaMap.CallAJO("keySet").CallAJO("iterator");
+			while (iterator.CallBool("hasNext"))
+			{
+				string key = iterator.CallStr("next");
+				var value = javaMap.CallAJO("get", key);
+				dictionary.Add(key, ParseJavaBoxedValue(value));
+			}
+
+			javaMap.Dispose();
+			return dictionary;
+		}
+
+		public static object ParseJavaBoxedValue(AndroidJavaObject boxedValueAjo)
+		{
+			if (boxedValueAjo == null || boxedValueAjo.IsJavaNull())
+			{
+				return null;
+			}
+
+			var className = boxedValueAjo.GetClassSimpleName();
+			switch (className)
+			{
+				case "Boolean":
+					return boxedValueAjo.CallBool("booleanValue");
+				case "Float":
+					return boxedValueAjo.CallFloat("floatValue");
+				case "Integer":
+					return boxedValueAjo.CallInt("intValue");
+				case "Long":
+					return boxedValueAjo.CallLong("longValue");
+				case "String":
+					return boxedValueAjo.CallStr("toString");
+			}
+			return boxedValueAjo;
+		}
+	}
+}
