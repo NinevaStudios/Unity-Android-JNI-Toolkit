d5ef5b7293e4836b04768152188ffdca7b792ff0
Taras Leskiv <tleskiv@transfollow.org>
2018-07-26 10:25:35 +0300

    added bitmap utils

---
+++ Scripts/BitmapUtils.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DeadMosquito.JniToolkit
+{
+	public static class BitmapUtils
+	{
+		const string AndroidGraphicsBitmapFactory = "android.graphics.BitmapFactory";
+		
+		public static AndroidJavaObject Texture2DToAndroidBitmap(this Texture2D tex2D, bool isPng = true)
+		{
+			var encoded = isPng ? tex2D.EncodeToPNG() : tex2D.EncodeToJPG();
+			return AndroidGraphicsBitmapFactory.AJCCallStaticOnce<AndroidJavaObject>("decodeByteArray", encoded, 0, encoded.Length);
+		}
+	}
+}
