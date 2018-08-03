629aa6ecfe779f02155810f9d161750d49ec0925
Ivan Tustanivsky <itustanivsky@ninevastudios.com>
2018-08-03 16:09:59 +0300

    add bitmap to texture conversion method

--- Scripts/BitmapUtils.cs
+++ Scripts/BitmapUtils.cs
@@ -11,5 +11,17 @@ namespace DeadMosquito.JniToolkit
 			var encoded = isPng ? tex2D.EncodeToPNG() : tex2D.EncodeToJPG();
 			return AndroidGraphicsBitmapFactory.AJCCallStaticOnce<AndroidJavaObject>("decodeByteArray", encoded, 0, encoded.Length);
 		}
+
+		public static Texture2D Texture2DFromBitmap(AndroidJavaObject bitmapAjo)
+		{
+			var compressFormatPng = new AndroidJavaClass("android.graphics.Bitmap$CompressFormat").GetStatic<AndroidJavaObject>("PNG");
+			var outputStream = new AndroidJavaObject("java.io.ByteArrayOutputStream");
+			bitmapAjo.CallBool("compress", compressFormatPng, 100, outputStream);
+			var buffer = outputStream.Call<byte[]>("toByteArray");
+
+			var tex = new Texture2D(2, 2);
+			tex.LoadImage(buffer);
+			return tex;
+		}
 	}
 }
